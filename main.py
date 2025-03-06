896bec0021039e68bbbc56be686f717f48121813
gaskeo <zeytunyan2004@mail.ru>
2025-03-06 14:50:53 +0500

    feat(service): fix cors

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.include_router(products_router.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
